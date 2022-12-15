b2c5eb83768142a3b07050728c2ceb2ce46a499b
mitisit89 <mitisit89@gmail.com>
2022-12-15 14:38:11 +0300

    write tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
@@ -10,3 +11,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve("/")
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_corretct_html(self):
+        requset = HttpRequest()
+        response = home_page(requset)
+        html = response.content.decode("utf8")
+        self.assertTrue(html.startswith("<html>"))
+        self.assertIn("<title>To-Do list</title>", html)
+        self.assertTrue(html.endswith("</html>"))
