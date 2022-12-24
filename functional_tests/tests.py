5094454c6fef1d1230a27b746bcdea35bf1bb5bc
mitisit89 <mitisit89@gmail.com>
2022-12-25 01:25:08 +0300

    Новый URL + представление для добавления в существующие списки. ФТ проходит :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -64,30 +64,29 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         # Satisfied, she goes back to sleep        # She visits that URL - her to-do list is still there.
+        def test_muliple_users_can_start_lists_at_different_urls(self):
+            self.browser.get(self.live_server_url)
+            inputbox = self.browser.find_element(By.ID, "id_new_item")
+            inputbox.send_keys("Buy peacock feathers")
+            inputbox.send_keys(Keys.ENTER)
+            self.wait_for_row_in_list_table("1: Buy peacock feathers")
+            edith_list_url = self.browser.current_url
+            self.assertRegex(edith_list_url, "/lists/.+")
+            self.browser.quit()
+            self.browser = webdriver.Firefox()
+            self.browser.get(self.live_server_url)
+            page_text = self.find_element(By.TAG_NAME, "body").text
+            self.assertNotIn("Buy peacock feathers", page_text)
+            self.assertNotIn("make a fly", page_text)
+            inputbox = self.browser.find_element(By.ID, "id_new_item")
+            inputbox.send_keys("Buy milk")
+            inputbox.send_keys(Keys.ENTER)
+            self.wait_for_row_in_list_table("1: Buy milk")
+            francis_list_url = self.browser.current_url
+            self.assertRegex(francis_list_url, "/lists/.+")
+            self.assertNotEqual(francis_list_url, edith_list_url)
 
-    def test_muliple_users_can_start_lists_at_different_urls(self):
-        self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
-        inputbox.send_keys("Buy peacock feathers")
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1: Buy peacock feathers")
-        edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, "/lists/.+")
-        self.browser.quit()
-        self.browser = webdriver.Firefox()
-        self.browser.get(self.live_server_url)
-        page_text = self.browser.find_element(By.TAG_NAME, "body").text
-        self.assertNotIn("Buy peacock feathers", page_text)
-        self.assertNotIn("make a fly", page_text)
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
-        inputbox.send_keys("Buy milk")
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1: Buy milk")
-        francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, "/lists/.+")
-        self.assertNotEqual(francis_list_url, edith_list_url)
-
-        # Again, there is no trace of Edith's list
-        page_text = self.browser.find_element(By.ID, "body").text
-        self.assertNotIn("Buy peacock feathers", page_text)
-        self.assertIn("Buy milk", page_text)
+            # Again, there is no trace of Edith's list
+            page_text = self.browser.find_element(By.ID, "body").text
+            self.assertNotIn("Buy peacock feathers", page_text)
+            self.assertIn("Buy milk", page_text)
